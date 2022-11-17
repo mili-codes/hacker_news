90ac35e380f8646a545833f68e39d6a323acf547
Mili Codes <mili_codes@outlook.com>
2022-11-17 21:29:01 +0530

    interview2

--- app/Interview2.py
+++ app/Interview2.py
@@ -10,15 +10,15 @@ with open(input_file, "r") as time_file:
     for line in file_line:
         l = line.split(":")
         epoch_timestamp = l[0]
-        print(epoch_timestamp)
+        print("Raw epoch timestamp: " + epoch_timestamp)
         # epoch_timestamp = "converted date time" inserting format conversion
         epoch_timestamp = datetime.fromtimestamp(float(epoch_timestamp), tz=timezone.utc)
         epoch_timestamp = epoch_timestamp.strftime("%a %d %b %H:%M:%S:%f000 %Z %Y")
+        print("Converted epoch timestamp: " + epoch_timestamp)
         l[0] = epoch_timestamp
         ol = ":".join(l)
         out_lines.append(ol)
     
-    print(out_lines)    
     with open(out_file, "w") as o_file:
         o_file.writelines(out_lines)
 
